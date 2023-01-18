104e7b783c8d5f1ee7e1c9d3b86109800d7de755
Autobuntu <rh166617@ohio.edu>
2023-01-18 14:41:43 -0500

    Data splitting Feature Add

--- python_rdf.py
+++ python_rdf.py
@@ -6,18 +6,26 @@ import os
 from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
 from sklearn import metrics
+from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import time
 import joblib
+from sklearn.metrics import confusion_matrix
 
 ################## OPTIONS ##################
 
 # Save Results
-rdf_save_bool = 1
+rdf_save_bool = 0
+
+# Split data
+split_training_data_bool = 1
+
+# Show Confusion Matrix
+show_conf_mat = 1
 
 # RDF options
 min_estimator = 5
-max_estimator = 305
+max_estimator = 205
 step_size = 5
 
 ################## VAR INIT ##################
@@ -33,28 +41,55 @@ parent_dir = "/media/autobuntu/chonk/chonk/git_repos/py_rdf_class/Py_Decision_Tr
 col_array = get_feats.get_feats()
 
 # Training Data Import
-td_df = pd.read_csv('Training_Data/RANSAC_TRAINING.csv')
-td_df_to_rdf = td_df[td_df.columns[col_array]]
-td_df_to_rdf = np.nan_to_num(td_df_to_rdf,nan= 0, posinf = 0, neginf=0)
-terrain_types_train = td_df[td_df.columns[-1]]
+train_data_all = pd.read_csv('Training_Data/RAN/RAN_BULK_ALL_&_MANUAL.csv')
 
-# Validation File Import
-vali_data_raw = pd.read_csv('Validation_Data/RANSAC_VALIDATION.csv')
-vali_data = vali_data_raw[vali_data_raw.columns[col_array]]
-vali_data = np.nan_to_num(vali_data,nan= 0, posinf = 0, neginf=0)
-terrain_types_vali = vali_data_raw[vali_data_raw.columns[-1]]
+# Split data - If needed, the total amount of data can be split into training/testing etc
+# Otherwise, it will load dedicated validation data seperate from the training data
+if split_training_data_bool:
+
+    terrain_types = train_data_all[train_data_all.columns[-1]]
+
+    # Splitting Training Data
+    train_data_all, vali_data_all, terrain_types_train, terrain_types_vali = train_test_split(train_data_all, terrain_types, test_size = 0.10, random_state=1)
+
+    # Handling Training Data
+    train_data = train_data_all[train_data_all.columns[col_array]]
+    train_data = np.nan_to_num(train_data,nan= 0, posinf = 0, neginf=0)
+
+    # Handling Validation Data
+    vali_data = vali_data_all[vali_data_all.columns[col_array]]
+    vali_data = np.nan_to_num(vali_data,nan= 0, posinf = 0, neginf=0)
+
+    print(train_data.shape)
+    print(vali_data.shape)
+
+else:
+
+    # Handling Training Data
+    train_data = train_data_all[train_data_all.columns[col_array]]
+    train_data = np.nan_to_num(train_data,nan= 0, posinf = 0, neginf=0)
+    terrain_types_train = train_data_all[train_data_all.columns[-1]]
+    
+    # Validation File Import
+    vali_data_all = pd.read_csv('Validation_Data/RANSAC_VALIDATION.csv')
+
+    # Handling Validation Data
+    vali_data = vali_data_all[vali_data_all.columns[col_array]]
+    vali_data = np.nan_to_num(vali_data,nan= 0, posinf = 0, neginf=0)
+    print(vali_data.shape)
+    terrain_types_vali = vali_data_all[vali_data_all.columns[-1]]
 
 # Creating the RDF parameters
 # Parameters are explained here - https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html
 # Principal differences from standard is the use of all cores (n_jobs = -1), verbose=1, max_depth=10,100,200, and warm_start=True.
 rdf_clf = RandomForestClassifier(n_estimators=100,  
 criterion='gini', 
-max_depth=35, 
+max_depth=50, 
 min_samples_split=2, 
 min_samples_leaf=1, 
 min_weight_fraction_leaf=0.0, 
 max_features='sqrt', 
-max_leaf_nodes=20, 
+max_leaf_nodes=None, 
 min_impurity_decrease=0.0, 
 bootstrap=True, 
 oob_score=True, 
@@ -68,6 +103,7 @@ max_samples=None)
 
 # Save locations
 if rdf_save_bool:
+
     rdf_export_dir = 'RDF_RAN_' + str(rdf_clf.max_depth) + str(time_now)
     rdf_export_path = os.path.join(parent_dir, rdf_export_dir)
     os.mkdir(rdf_export_path)
@@ -78,7 +114,7 @@ for i in range(min_estimator, max_estimator+1, step_size):
 
     # Training
     rdf_clf.set_params(n_estimators = i)
-    rdf_clf.fit(td_df_to_rdf, terrain_types_train)
+    rdf_clf.fit(train_data, terrain_types_train)
 
     # Testing
     oob_error = 1 - rdf_clf.oob_score_
@@ -92,6 +128,10 @@ for i in range(min_estimator, max_estimator+1, step_size):
     if rdf_save_bool:
         rdf_pickle_name = "RDF_" + str(rdf_clf.n_estimators) + "_" + str(rdf_clf.max_depth) + ".joblib"
         joblib.dump(rdf_clf, os.path.join(rdf_export_path,rdf_pickle_name))
+
+    if show_conf_mat:
+        conf_mat = confusion_matrix(terrain_types_vali, y_pred)
+        print(conf_mat)
 
 # Saving the validation results
 if rdf_save_bool:
@@ -110,6 +150,7 @@ plt.plot(oob_x_coords, oob_y_coords)
 plt.plot(vali_x_coords, vali_y_coords)
 plt.xlabel('n_estimators')
 plt.ylabel('Error %')
+plt.legend()
 plt.show()
 
 # Save Plot
